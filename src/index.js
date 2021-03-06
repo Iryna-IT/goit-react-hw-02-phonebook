46f7da7685864b3465a17001e841cc0c62a0863c
Iryna-IT <redkaiv@gmail.com>
2021-03-06 03:47:35 +0200

    settings for formating code

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import App from './components/App';
 
 import 'modern-normalize';
 
-
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
